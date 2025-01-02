edd5b2a6995609fee22e108505299dc5702cc675
김선우 <frod5@kakao.com>
2025-01-02 18:33:41 +0900

    nest js 스터디 프로젝트 - Get 요청해보기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,17 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHome() {
+    return 'home page';
+  }
+
+  @Get('post')
+  getPost() {
+    return 'post page';
+  }
+
+  @Get('user')
+  getUser() {
+    return 'user page';
   }
 }
