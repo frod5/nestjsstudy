e2568cd83686e00b7b2b9913b146c0f188435eb1
김선우 <frod5@kakao.com>
2025-01-02 19:35:30 +0900

    nest js 스터디 프로젝트 - JSON 반환하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHome() {
-    return 'home page';
-  }
-
   @Get('post')
-  getPost() {
-    return 'post page';
-  }
-
-  @Get('user')
-  getUser() {
-    return 'user page';
+  getPost(): Post {
+    return {
+      author: 'autor1',
+      title: '제목',
+      content: '내용',
+      likeCount: 100,
+      commentCount: 9999,
+    };
   }
 }
