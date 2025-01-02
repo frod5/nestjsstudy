627b287aa7b3590ba3709936e909df8b6c94359d
김선우 <frod5@kakao.com>
2025-01-02 21:37:42 +0900

    nest js 스터디 프로젝트 - typeORM 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      //데이터베이스 타입
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true, // 개발환경에서만 true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
