1fb38408c8c51626502ba01179e3184a5e40398f
김선우 <frod5@kakao.com>
2025-01-03 22:17:50 +0900

    nest js 스터디 프로젝트 - typeORM 스터디 - find options

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -21,10 +21,42 @@ export class UsersService {
 
   async findAll() {
     /*return this.userRepository.find({
+      //어떤 프로퍼티를 선택할지
+      //기본은 모든 프로퍼티를 가져온다
+      //select를 정의하면 정의한 프로퍼티만 가져온다.
       select: {
         id: true,
         title: true,
+        profile: {
+          id: true,
+        }
       },
+
+      //필터링할 조건을 정의
+      //기본적으로 and 연산
+      //OR를 하고싶으면 리스트에 담으면된다
+      //where: [
+      //  {id:1},{version:2}
+      //  profile: {id:3}
+      //]
+      where:{
+
+      }
+
+      //관계를 가져오는 법
+      //select, where안에도 가능
+      relations: {}
+
+      //order
+      order:{
+        id: 'ASC',
+      }
+
+      //처음 몇개를 제외할지
+      skip:{}
+
+      //데이터 갯수
+      take: 1,
     });*/
 
     return this.userRepository.find({
