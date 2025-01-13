61028d012e7bb4e28aa36dfd8a2dd3184677f4c3
김선우 <frod5@kakao.com>
2025-01-13 18:02:00 +0900

    nest js 스터디 프로젝트 - study용 route도 public 작업

--- src/study/study.controller.ts
+++ src/study/study.controller.ts
@@ -1,67 +1,80 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { StudyService } from './study.service';
+import {IsPublic} from "../common/decorator/is-public.decorator";
 
 @Controller('study')
 export class StudyController {
   constructor(private readonly usersService: StudyService) {}
 
   @Get()
+  @IsPublic()
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @IsPublic()
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Post()
+  @IsPublic()
   create(@Body('title') title: string) {
     return this.usersService.create(title);
   }
 
   @Patch(':id')
+  @IsPublic()
   update(@Param('id') id: string, @Body('title') title: string) {
     return this.usersService.patch(+id, title);
   }
 
   @Delete(':id')
+  @IsPublic()
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
 
   @Post('profile')
+  @IsPublic()
   createUserAndProfile() {
     return this.usersService.createUserAndProfile();
   }
 
   @Post('/user/car')
+  @IsPublic()
   createUserAndCar() {
     return this.usersService.createUserAndCar();
   }
 
   @Post('/tags/cars')
+  @IsPublic()
   createTagAndCar() {
     return this.usersService.createTagAndCar();
   }
 
   @Get('/user/cars')
+  @IsPublic()
   getCars() {
     // return 'test';
     return this.usersService.getAllCars();
   }
 
   @Get('/user/tags')
+  @IsPublic()
   getTags() {
     return this.usersService.getAllTags();
   }
 
   @Delete('profile/:id')
+  @IsPublic()
   deleteProfile(@Param('id') id: string) {
     return this.usersService.deleteProfile(+id);
   }
 
   @Post('sample')
+  @IsPublic()
   sample() {
     return this.usersService.sample();
   }
